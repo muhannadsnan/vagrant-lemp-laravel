e712bebb78ec5753bef4ca0d5a24fc8addf04111
muhannadsnan <muhannad@cloudmaster.no>
2020-08-25 15:37:15 +0200

    terminal: hiden commands, just my messages

--- provision-centos.sh
+++ provision-centos.sh
@@ -1,33 +1,33 @@
 echo "----------------- Welcome to my Vagrant! -----------------"
-yum update
-yum install -y nano
+yum update &> /dev/null
+yum install -y nano &> /dev/null
 
 echo "----------------- Step 1: Install & configure nginx -----------------"
-yum install -y nginx
+yum install -y nginx &> /dev/null
 cd /etc/nginx/
 mkdir sites-available sites-enabled
 cp /vagrant/dev.local.conf sites-available/dev.local.conf
 cp sites-available/dev.local.conf conf.d/dev.local.conf
-ln -fs sites-available/dev.local.conf sites-enabled
+ln -fs sites-available/dev.local.conf sites-enabled &> /dev/null
 
 echo "----------------- Step 2: Adjust Firewall Rules -----------------"
 #adjust the firewall settings in order to allow external connections on your Nginx web server, which runs on port 80 by default.
 #setenforce permissive
-firewall-cmd --permanent --add-service=http
-firewall-cmd --permanent --list-all
-firewall-cmd --reload
+firewall-cmd --permanent --add-service=http &> /dev/null
+firewall-cmd --permanent --list-all &> /dev/null
+firewall-cmd --reload &> /dev/null
 
 echo "----------------- Step 3: Install & configure php -----------------"
-yum install -y php php-fpm php-mysqlnd
+yum install -y php php-fpm php-mysqlnd &> /dev/null
 sudo cp /vagrant/php-fpm--www.conf /etc/php-fpm.d/www.conf
 cp /vagrant/php.ini /etc/php.ini
 
 echo "----------------- Step 4: Install & secure & configure mysql -----------------"
-yum install -y mariadb mariadb-server
-systemctl enable mariadb
-systemctl start mariadb
-systemctl status mariadb
-yum install -y expect
+yum install -y mariadb mariadb-server &> /dev/null
+systemctl enable mariadb &> /dev/null
+systemctl start mariadb &> /dev/null
+systemctl status mariadb &> /dev/null
+yum install -y expect &> /dev/null
 MYSQL_ROOT_PASSWORD=12345
 expect -c "set timeout 10
 spawn mysql_secure_installation
@@ -47,32 +47,32 @@ expect \"Remove test database and access to it?\"
 send \"y\r\"
 expect \"Reload privilege tables now?\"
 send \"y\r\"
-expect eof"
+expect eof" &> /dev/null
 
-mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "SHOW DATABASES; CREATE DATABASE VAGRANT_DB; EXIT;"
-yum remove -y expect
+mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "SHOW DATABASES; CREATE DATABASE VAGRANT_DB; EXIT;" &> /dev/null
+yum remove -y expect &> /dev/null
 
 echo "----------------- Install & configure phpmyadmin -----------------"
-yum install -y php-json php-mbstring
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-all-languages.tar.gz
-tar -zxvf phpMyAdmin-5.0.1-all-languages.tar.gz
+yum install -y php-json php-mbstring &> /dev/null
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-all-languages.tar.gz &> /dev/null
+tar -zxvf phpMyAdmin-5.0.1-all-languages.tar.gz &> /dev/null
 mv phpMyAdmin-5.0.1-all-languages /usr/share/phpMyAdmin
-cp -pr /usr/share/phpMyAdmin/config.sample.inc.php /usr/share/phpMyAdmin/config.inc.php
+cp -pr /usr/share/phpMyAdmin/config.sample.inc.php /usr/share/phpMyAdmin/config.inc.php &> /dev/null
 cp /vagrant/conf.d--phpmyadmin.conf /etc/nginx/conf.d/phpmyadmin.conf
 cp /vagrant/phpmyadmin--config.inc.php /usr/share/phpMyAdmin/config.inc.php
 mkdir /usr/share/phpMyAdmin/tmp
-chmod 777 /usr/share/phpMyAdmin/tmp
-chmod 777 /var/lib/php/session/
-chown -R nginx:nginx /usr/share/phpMyAdmin
-mysql < /usr/share/phpMyAdmin/sql/create_tables.sql -u root --password=$MYSQL_ROOT_PASSWORD
+chmod 777 /usr/share/phpMyAdmin/tmp &> /dev/null
+chmod 777 /var/lib/php/session/ &> /dev/null
+chown -R nginx:nginx /usr/share/phpMyAdmin &> /dev/null
+mysql < /usr/share/phpMyAdmin/sql/create_tables.sql -u root --password=$MYSQL_ROOT_PASSWORD &> /dev/null
 
 
 echo "----------------- Starting & enabling services NGINX & PHP-FPM -----------------";
-systemctl start nginx
-systemctl start php-fpm
-systemctl start mysql
-systemctl enable nginx
-systemctl enable php-fpm
+systemctl start nginx &> /dev/null
+systemctl start php-fpm &> /dev/null
+systemctl start mysql &> /dev/null
+systemctl enable nginx &> /dev/null
+systemctl enable php-fpm &> /dev/null
 
 echo "----------------- Done! -----------------"
 
